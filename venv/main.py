8bd38d34213ced652ab511d0517dbd47953333a9
Dan <daniel.iwanicki@protonmail.com>
2019-03-17 01:11:20 +0100

    nowa wersja

--- venv/main.py
+++ venv/main.py
@@ -1,13 +1,58 @@
 from tkinter import *
 import tkinter.ttk as ttk
 from EntryNum import EntryNum
+import re
 
+
+def mouse_click(event):
+    x = event.x
+    y = event.y
+    r = 3
+    width = entry_width.getNum()*tile_size
+    height = entry_height.getNum()*tile_size
+    if x > width:
+        x = width
+    if y > height:
+        y = height
+    x = round(x/tile_size)
+    y = round(y/tile_size)
+    canvas.create_oval(x*tile_size-r, y*tile_size-r, x*tile_size+r, y*tile_size+r, fill='blue')
+    print('pozycja:', x, y)
+
+
+def button_click():
+    text = entry.get()
+    listbox.insert(END, text)
+    # paczki = re.split("; ", text)
+    paczki = re.findall("[0-9], [0-9]", text)
+    print(paczki)
+    entry.delete(0, len(text))
+
+
+def button_size_click():
+    width = entry_width.getNum()
+    height = entry_height.getNum()
+    canvas.delete("all")
+    canvas.create_rectangle(0, 0, width*tile_size, height*tile_size, outline="#fb0", fill="#fb0")
+
+    for row in range(width):
+        canvas.create_line(row*tile_size, 0, row*tile_size, height*tile_size)
+
+    for col in range(height):
+        canvas.create_line(0, col * tile_size, width * tile_size, col * tile_size)
+
+
+# definicje zmiennych
 root = Tk()
 ttk.Style().theme_use('xpnative')
 
 tile_size = 20
+paczki = []  #lista rozmiarow paczek
 
-# RIGHT
+x = 0
+y = 0
+
+# RIGHT - wprowadzanie paczek
 
 rightFrame = Frame(root)
 rightFrame.pack(side=RIGHT, fill=Y)
@@ -18,18 +63,13 @@ rightUpFrame.pack(side=TOP)
 entry = ttk.Entry(rightUpFrame)
 entry.pack(side=LEFT)
 
-def button_click():
-    text = entry.get()
-    listbox.insert(END, text)
-    entry.delete(0, len(text))
-
 button = ttk.Button(rightUpFrame, text="DODAJ", command=button_click)
 button.pack()
 
 listbox = Listbox(rightFrame)
 listbox.pack(side=LEFT, fill=Y)
 
-# LEFT
+# LEFT - magazyn
 
 leftFrame = Frame(root)
 leftFrame.pack(fill=BOTH, expand=YES)
@@ -47,23 +87,12 @@ label_height.pack(side=LEFT)
 entry_height = EntryNum(leftUpFrame, text='10')
 entry_height.pack(side=LEFT)
 
-def button_size_click():
-    width = entry_width.getNum()
-    height = entry_height.getNum()
-    canvas.delete("all")
-    canvas.create_rectangle(0, 0, width*tile_size, height*tile_size, outline="#fb0", fill="#fb0")
-
-    for row in range(width):
-        canvas.create_line(row*tile_size, 0, row*tile_size, height*tile_size)
-
-    for col in range(height):
-        canvas.create_line(0, col * tile_size, width * tile_size, col * tile_size)
-
-
 button_size = ttk.Button(leftUpFrame, text="Apply", command=button_size_click)
 button_size.pack(side=LEFT)
 
 canvas = Canvas(leftFrame)
+
+canvas.bind("<Button-1>", mouse_click)
 canvas.pack(side=BOTTOM, fill=BOTH, expand=YES)
 
 button_size_click()
